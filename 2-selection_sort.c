a7bc7a29597fd3311bc6636588760975049dd295
OctopusHugz <colsonscott53@gmail.com>
2020-06-16 22:32:49 +0000

    Updated solution for exercise 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,12 +8,15 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int index = 0, temp = 0, min = 0, mindex, searcher;
+	int index = 0, temp = 0, min = 0, sorted = 0, mindex, searcher;
 
 	if (array == NULL || size < 2)
 		return;
-	for (; array[index]; index++)
+	for (; array[index + 1]; index++)
 	{
+		sorted = check_array(array);
+		if (sorted)
+			return;
 		for (searcher = index, min = array[index]; array[searcher]; searcher++)
 		{
 			if (array[searcher] < min)
@@ -30,4 +33,23 @@ void selection_sort(int *array, size_t size)
 			print_array(array, size);
 		}
 	}
+}
+
+/**
+ * check_array - checks to see if an array is sorted or not
+ * @array: pointer to array of integers to selection sort
+ * Return: 1 if array is sorted, 0 if unsorted
+ **/
+
+int check_array(int *array)
+{
+	int index = 0;
+
+	while (array[index + 1])
+	{
+		if (array[index] > array[index + 1])
+			return (0);
+		index++;
+	}
+	return (1);
 }
