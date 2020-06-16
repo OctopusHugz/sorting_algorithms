515ebf5df089f84923435d170bc7eee1b2800149
OctopusHugz <colsonscott53@gmail.com>
2020-06-16 21:28:21 +0000

    Removed comments and moved print_array to where swapping occured

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,13 +8,11 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int index = 0, temp = 0, /* sorted = 0,  */ min = 0, mindex, searcher;
-	/* size_t count = 1; */
+	int index = 0, temp = 0, min = 0, mindex, searcher;
 
 	if (array == NULL || size < 2)
 		return;
-	/* min = array[0]; */
-	for (; array[index + 2]; index++)
+	for (; array[index]; index++)
 	{
 		for (searcher = index, min = array[index]; array[searcher]; searcher++)
 		{
@@ -29,37 +27,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[index];
 			array[index] = min;
 			array[mindex] = temp;
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
-
-	/* while (!sorted)
-	{
-		index = 0;
-		min = array[swindex];
-		while (array[index])
-		{
-			if (array[index] < min)
-			{
-				min = array[index];
-				mindex = index;
-			}
-			if (array[index + 1] == '\0')
-			{
-				temp = array[swindex];
-				array[swindex] = min;
-				array[mindex] = temp;
-				print_array(array, size);
-				min = array[swindex];
-				swindex++;
-				index = 0;
-				count++;
-				break;
-			}
-			index++;
-		}
-
-		if (count == size)
-			sorted = 1;
-	} */
 }
