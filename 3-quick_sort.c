b6e6a5596b4fd1940ba9613a074f303c82b0c652
OctopusHugz <colsonscott53@gmail.com>
2020-06-17 17:43:21 +0000

    Added outline for exercise 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers using quick sort algorithm
+ * @array: pointer to array of integers to quick sort
+ * @size: size of array
+ **/
+
+void quick_sort(int *array, size_t size)
+{
+	(void)array, (void)size;
+}
